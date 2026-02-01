c02a551227835cbfb1f261962fe7e330a890fe81
ajithkumar144 <ajith28062002@gmail.com>
2026-02-01 16:31:12 +0530

    copyright added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,9 +6,9 @@ const Footer = () => {
             <div className="footer-content">
                 <p>&copy; {new Date().getFullYear()} Shopzi. All rights reserved.</p>
                 <div className="footer-links">
-                    <span>Privacy Policy</span>
-                    <span>Terms of Service</span>
-                    <span>Contact Us</span>
+                    <span>Designed by Ajithkumar</span>
+                    {/* <span>Terms of Service</span>
+                    <span>Contact Us</span> */}
                 </div>
             </div>
         </footer>
